1f846d4b7ab66b8f4690296c2fb270ff1d6838ee
wslnn <lygs72656@163.com>
2020-12-28 16:47:33 +0800

    12月28日提交更新，使用es完成了社区搜索功能的开发，当前存在的问题是在搜索界面上取不到帖子标题，标题处取到了正文内容

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -60,7 +60,14 @@ public interface CommunityConstant {
     String TOPIC_FOLLOW="follow";
 
     /**
+     * 主题:发帖
+     */
+    String TOPIC_PUBLISH="publish";
+
+    /**
      * 系统用户的id
      */
     int SYSTEM_USERID=1;
+
+
 }
